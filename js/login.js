9c2fab14c24a46d597ab991ced062d218dad53b2
AbsintheSung <h90243768@gmail.com>
2024-05-25 00:58:53 +0800

    [新增]-串接登入API以及驗證TOKEN成功

--- js/login.js
+++ js/login.js
@@ -5,19 +5,47 @@ const inputPassword = document.querySelector(".login-password")
 const loginButton = document.querySelector(".login-button")
 
 const userInput = {
-    userEmail: "",
-    userPassword: "",
+    email: "",
+    password: "",
 }
 
 
 inputEmail.addEventListener("input", (event) => {
-    userInput.userEmail = event.target.value
+    userInput.email = event.target.value
 })
 inputPassword.addEventListener("input", (event) => {
-    userInput.userPassword = event.target.value
+    userInput.password = event.target.value
 })
 loginButton.addEventListener("click", async (event) => {
     event.preventDefault();
-    console.log(userInput.userEmail, userInput.userPassword)
+    fetchUserData(userInput)
 })
+
+const fetchUserData = async (userdata) => {
+    const fetchData = { "user": { ...userdata } }
+    try {
+        const response = await axios.post("https://todoo.5xcamp.us/users/sign_in", fetchData);
+        if (response.status === 200) {
+            document.cookie = `TokenCode=${response.headers["authorization"]}`
+        }
+        console.log(response)
+    } catch (error) {
+        console.error(error.response);
+    }
+}
+
+//測試授權
+const checkToken = async () => {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)TokenCode\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    try {
+        const response = await axios.get("https://todoo.5xcamp.us/check", {
+            headers: {
+                'Authorization': token
+            }
+        });
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
