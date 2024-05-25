a46afebc136f24e0fe66bf8c51b706e75f0a7d83
AbsintheSung <h90243768@gmail.com>
2024-05-25 22:25:05 +0800

    [修改]-修改取值的監聽方式

--- js/login.js
+++ js/login.js
@@ -1,30 +1,11 @@
 import axios from 'axios'
-
 const inputEmail = document.querySelector(".login-email")
 const inputPassword = document.querySelector(".login-password")
 const loginButton = document.querySelector(".login-btn")
 
-const userInput = {
-    email: "",
-    password: "",
-}
-
-
-inputEmail.addEventListener("input", (event) => {
-    userInput.email = event.target.value
-})
-inputPassword.addEventListener("input", (event) => {
-    userInput.password = event.target.value
-})
-loginButton.addEventListener("click", async (event) => {
-    event.preventDefault();
-    fetchUserData(userInput)
-})
-
 const fetchUserData = async (userdata) => {
-    const fetchData = { "user": { ...userdata } }
     try {
-        const response = await axios.post("https://todoo.5xcamp.us/users/sign_in", fetchData);
+        const response = await axios.post("https://todoo.5xcamp.us/users/sign_in", userdata);
         if (response.status === 200) {
             document.cookie = `TokenCode=${response.headers["authorization"]}`
         }
@@ -48,4 +29,16 @@ const checkToken = async () => {
         console.log(error)
     }
 }
+
+loginButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    const userInput = {
+        "user": {
+            email: inputEmail.value,
+            password: inputPassword.value
+        }
+    }
+    await fetchUserData(userInput)
+})
+
 
