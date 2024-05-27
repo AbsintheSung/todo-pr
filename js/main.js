145015a02e990ab07cee6877f086f8a7bdfb1ecf
AbsintheSung <h90243768@gmail>
2024-05-27 14:35:53 +0800

    [新增]-撰寫刪除提示

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 import createElementLi from "./li_module"
-import { handleEdit } from "./swal"
+import { handleEdit,handleDelete } from "./swal"
 const token = document.cookie.replace(/(?:(?:^|.*;\s*)TokenCode\s*\=\s*([^;]*).*$)|^.*$/, "$1"); //獲取存在cookie的token
 const fragment = document.createDocumentFragment()
 const todoList = document.querySelector('.todo-list')
